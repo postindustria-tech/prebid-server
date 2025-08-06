e05643eda2e89e482a04788d04e177b3a0f4be92
postindustria-code <oss@postindustria.com>
2025-08-06 17:51:26 +0300

    Test files

---
+++ adapters/boldwin_rapid/params_test.go
@@ -0,0 +1,21 @@
+package boldwin_rapid
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/prebid/prebid-server/v3/openrtb_ext"
+)
+
+func TestValidParams(t *testing.T) {
+	validator, err := openrtb_ext.NewBidderParamsValidator("../../static/bidder-params")
+	if err != nil {
+		t.Fatalf("Failed to fetch the json schema. %v", err)
+	}
+
+	for _, p := range validParams {
+		if err := validator.Validate(openrtb_ext.BidderBoldwinRapid, json.RawMessage(p)); err != nil {
+			t.Errorf("Schema rejected valid params: %s", p)
+		}
+	}
+}
